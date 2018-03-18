ce6e140a4dbff51d632601e5f62e1ad0381346ea
Róbert Fuszenecker <fuszenecker@localhost.localdomain>
2018-03-18 19:02:46 +0100

    Set up the logger a bit better

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,24 @@
 extern crate log;
 extern crate env_logger;
 
+use std::env;
+use env_logger::{Builder, Target};
+
+fn configure_logger() {
+    let mut builder = Builder::new();
+
+    builder.target(Target::Stdout);
+
+    if !env::var("RUST_LOG").is_ok() {
+        builder.parse("info");
+    }
+
+    builder.init();
+}
+
 fn main() {
-    env_logger::init();
+    configure_logger();
+
     info!("Yalci is starting...");
 
     info!("Yalci has started");
